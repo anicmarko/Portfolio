3848dd3e2850bc26521f753429477a3d39859c7d
Marko Anic <anic.it36.2022@gmail.com>
2025-05-28 13:49:23 +0200

    feat: add Header component, configure Tailwind CSS for custom fonts, update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga} from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: "400",
+});
 
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description: "Marko Anić - Portfolio Website",
 };
 
 export default function RootLayout({
@@ -13,7 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body 
+        className={
+        twMerge(
+          inter.variable, 
+          calistoga.variable, 
+          "bg-gray-900 text-white antialiased font-sans"
+          )}
+        >{children}
+        </body>
     </html>
   );
 }
