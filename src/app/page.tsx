61e1bd71801913f68477819d93ca1618a5226b42
Marko Anic <anic.it36.2022@gmail.com>
2025-05-29 13:33:02 +0200

    feat: add Footer component with navigation links and copyright information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <ProjectsSection />
       <TapeSection />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
