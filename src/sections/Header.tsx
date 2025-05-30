124dc29a6cc61d2270790e73370e188079c1e31b
Marko Anic <anic.it36.2022@gmail.com>
2025-05-30 17:18:23 +0200

    feat: increase z-index of Header component for improved visibility

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (  
-    <div className="flex justify-center items-center fixed top-3 z-10 w-full">
+    <div className="flex justify-center items-center fixed top-3 z-20 w-full">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <Link href="#home" className="nav-item">Home</Link>
         <Link href="#projects" className="nav-item">Projects</Link>
