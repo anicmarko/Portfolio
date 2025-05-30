e1b6728718674025364df8aa428b8af84af578c5
Marko Anic <anic.it36.2022@gmail.com>
2025-05-30 16:54:11 +0200

    feat: update project images and details in Projects section; remove outdated images and add new projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,6 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import OnlyAiImage from "@/assets/images/onlyai.png";
+import DallEImage from "@/assets/images/dalle.png";
+import ARShopImage from "@/assets/images/arshop.png";
 import Link from "next/link";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
@@ -10,40 +10,40 @@ import Card from "@/components/Card";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "Freelance",
+    year: "2023",
+    title: "OnlyAI",
     results: [
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://only-ai.vercel.app/",
+    image: OnlyAiImage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "Freelance",
+    year: "2023",
+    title: "Dall·E Clone",
     results: [
       { title: "Boosted sales by 20%" },
       { title: "Expanded customer reach by 35%" },
       { title: "Increased brand awareness by 15%" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    link: "https://dall-e-clone-teal.vercel.app/",
+    image: DallEImage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
+    company: "Freelance",
+    year: "2024",
+    title: "ARShop",
     results: [
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    link: "https://ar-shop-store.vercel.app/",
+    image: ARShopImage,
   },
 ];
 
